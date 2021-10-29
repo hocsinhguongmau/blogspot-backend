d821da0789d22b8ccfa95182f8b67d02c7252a4d
Thang Nguyen <thangnguyen24111990@gmail.com>
2021-10-29 14:52:26 +0300

    Push code

--- schemas/post.js
+++ schemas/post.js
@@ -21,7 +21,7 @@ export default {
       name: 'author',
       title: 'Author',
       type: 'reference',
-      to: {type: 'author'},
+      to: { type: 'author' },
     },
     {
       name: 'mainImage',
@@ -35,7 +35,13 @@ export default {
       name: 'categories',
       title: 'Categories',
       type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
+      of: [{ type: 'reference', to: { type: 'category' } }],
+    },
+    {
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [{ type: 'reference', to: { type: 'tag' } }],
     },
     {
       name: 'publishedAt',
@@ -47,6 +53,36 @@ export default {
       title: 'Body',
       type: 'blockContent',
     },
+    //Add SEO tool
+    // {
+    //   name: 'seo',
+    //   title: 'SEO',
+    //   type: 'seo-tools', // use seo-tools type
+    //   options: {
+    //     baseUrl: 'https://.../', // (REQUIRED) This is the baseUrl for your site
+    //     baseUrl(doc) {
+    //       return 'https://.../' // for dynamic baseUrls
+    //     },
+    //     slug(doc) {
+    //       // (REQUIRED) a function to return the sug of the current page, which will be appended to the baseUrl
+    //       return doc.slug.current
+    //     },
+    //     fetchRemote: true, // Can be set to false to disable fetching the remote source (you will need to pass the content helpers for analysis)
+    //     content(doc) {
+    //       return 'simple html representation of your doc' // (OPTIONAL) If your site is generated after Sanity content updates you can use this for better real time feedback
+    //     },
+    //     title(doc) {
+    //       return 'page title' // (OPTIONAL) return page title otherwise inferred from scrape
+    //     },
+    //     description(doc) {
+    //       return 'page description' // (OPTIONAL) return page description otherwise inferred from scrape
+    //     },
+    //     locale(doc) {
+    //       return 'page locale' // (OPTIONAL) return page locale otherwise inferred from scrape
+    //     },
+    //     contentSelector: 'body', // (OPTIONAL) option to finetune where Yoast will look for the content. (only applicable for scraping without content function)
+    //   },
+    // },
   ],
 
   preview: {
@@ -56,7 +92,7 @@ export default {
       media: 'mainImage',
     },
     prepare(selection) {
-      const {author} = selection
+      const { author } = selection
       return Object.assign({}, selection, {
         subtitle: author && `by ${author}`,
       })
